a26b7b8548924a9e897b600a2c7e2649a2636628
nimkoes <xxxelppa@gmail.com>
2021-06-26 22:58:29 +0900

    Servlet WebApplicationContext setting

---
+++ src/main/java/me/nimkoes/MyHelloService.java
@@ -0,0 +1,12 @@
+package me.nimkoes;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyHelloService {
+
+    public String getName() {
+        return "nimkoes";
+    }
+
+}
---
+++ src/main/java/me/nimkoes/MyWebConfig.java
@@ -0,0 +1,10 @@
+package me.nimkoes;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class MyWebConfig {
+
+}
