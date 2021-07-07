1333b838f601503bc871c2daf2f26cd2c0c4662e
nimkoes <xxxelppa@gmail.com>
2021-07-08 07:03:28 +0900

    2.5 리소스 핸들러 (default Servlet)

--- src/main/java/me/nimkoes/WebConfig.java
+++ src/main/java/me/nimkoes/WebConfig.java
@@ -1,7 +1,10 @@
 package me.nimkoes;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
             .addPathPatterns("/hi")
             .order(0);
         registry.addInterceptor(new AnotherInterceptor()).order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")            // 어떤 패턴의 요청을 처리할지 정의
+            .addResourceLocations("classpath:/m/", "file:/Users/nimkoes/files/", "/myStatic/")  // 리소스를 찾을 위치
+            .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));  // 리소스가 변경되지 않은 동안 캐싱을 유지할 시간
     }
 }
--- src/main/test/me/nimkoes/MyHelloControllerTest.java
+++ src/main/test/me/nimkoes/MyHelloControllerTest.java
@@ -2,12 +2,14 @@ package me.nimkoes;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,4 +30,20 @@ public class MyHelloControllerTest {
             .andExpect(content().string("hello nimkoes"));
     }
 
+    @Test
+    public void staticTest() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(Matchers.containsString("Hello Nimkoes")));
+    }
+
+    @Test
+    public void staticTest_M() throws Exception {
+        this.mockMvc.perform(get("/m/index.html"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(Matchers.containsString("Hello Mobile Nimkoes")))
+            .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+    }
 }
