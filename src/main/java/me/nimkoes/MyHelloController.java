8ca8a99f9183367ae4f666a1697871f2bcdb7def
nimkoes <xxxelppa@gmail.com>
2021-07-10 23:32:30 +0900

    3.1 요청 매핑하기 (handler method) part 1

--- src/main/java/me/nimkoes/MyHelloController.java
+++ src/main/java/me/nimkoes/MyHelloController.java
@@ -1,8 +1,25 @@
 package me.nimkoes;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(method = RequestMethod.GET)
 public class MyHelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping
+    @ResponseBody
+    public String hi() {
+        return "hi";
+    }
 
 }
--- src/main/test/me/nimkoes/MyHelloControllerTest.java
+++ src/main/test/me/nimkoes/MyHelloControllerTest.java
@@ -1,11 +1,30 @@
 package me.nimkoes;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class MyHelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+
+        mockMvc.perform(get("/hello")).andExpect(status().isOk());                 // 200
+        mockMvc.perform(post("/hello")).andExpect(status().isMethodNotAllowed());  // 405
+        mockMvc.perform(get("/hi")).andExpect(status().isNotFound());              // 404
+        mockMvc.perform(post("/hi")).andExpect(status().isNotFound());             // 404
+
+    }
 
 }
