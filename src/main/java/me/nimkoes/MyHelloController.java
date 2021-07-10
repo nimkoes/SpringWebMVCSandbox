2f0403ccb0c7e7b6b97d9d3dc81b8741ec42166b
nimkoes <xxxelppa@gmail.com>
2021-07-11 01:30:17 +0900

    3.1 요청 매핑하기 (handler method) part 3

--- src/main/java/me/nimkoes/MyHelloController.java
+++ src/main/java/me/nimkoes/MyHelloController.java
@@ -1,36 +1,21 @@
 package me.nimkoes;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class MyHelloController {
 
-    @GetMapping("/hello?")
-    public String hello_case_1() { return "hello case 1"; }
-
-    @GetMapping("/hello/?")
-    public String hello_case_2() { return "hello case 2"; }
-
-    @GetMapping("/hello/??")
-    public String hello_case_3() { return "hello case 3"; }
-
-    @GetMapping("/hello/?/?")
-    public String hello_case_4() { return "hello case 4"; }
-
-    @GetMapping("/hello*")
-    public String hello_case_5() { return "hello case 5"; }
-
-    @GetMapping("/hello/*/test")
-    public String hello_case_6() { return "hello case 6"; }
-
-    @GetMapping("/hello/*/te*st")
-    public String hello_case_7() { return "hello case 7"; }
-
-    @GetMapping("/hello/**")
-    public String hello_case_8() { return "hello case 8"; }
-
-    @GetMapping("/hello/**/test")
-    public String hello_case_9() { return "hello case 9"; }
+    @GetMapping(
+        value = "/hello",
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
 
 }
--- src/main/test/me/nimkoes/MyHelloControllerTest.java
+++ src/main/test/me/nimkoes/MyHelloControllerTest.java
@@ -1,12 +1,14 @@
 package me.nimkoes;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,18 +22,15 @@ public class MyHelloControllerTest {
     @Test
     public void helloTest() throws Exception {
 
-        mockMvc.perform(get("/hello1"))                    .andExpect(content().string("hello case 1"));
-        mockMvc.perform(get("/hello!"))                    .andExpect(content().string("hello case 1"));
-        mockMvc.perform(get("/hello/z"))                   .andExpect(content().string("hello case 2"));
-        mockMvc.perform(get("/hello/nk"))                  .andExpect(content().string("hello case 3"));
-        mockMvc.perform(get("/hello/n/k"))                 .andExpect(content().string("hello case 4"));
-        mockMvc.perform(get("/hello"))                     .andExpect(content().string("hello case 5"));
-        mockMvc.perform(get("/helloTest"))                 .andExpect(content().string("hello case 5"));
-        mockMvc.perform(get("/hello/nimkoes/test"))        .andExpect(content().string("hello case 6"));
-        mockMvc.perform(get("/hello/nk/te_some_st"))       .andExpect(content().string("hello case 7"));
-        mockMvc.perform(get("/hello/nk/some"))             .andExpect(content().string("hello case 8"));
-        mockMvc.perform(get("/hello/nk/some/another"))     .andExpect(content().string("hello case 8"));
-        mockMvc.perform(get("/hello/nk/some/another/test")).andExpect(content().string("hello case 9"));
+        mockMvc.perform(
+                get("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.TEXT_PLAIN)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string("hello"))
+        ;
 
     }
 
