580274c483685944b37982327263a24c7d3422aa
nimkoes <xxxelppa@gmail.com>
2021-07-11 00:19:19 +0900

    3.1 요청 매핑하기 (handler method) part 2

--- src/main/java/me/nimkoes/MyHelloController.java
+++ src/main/java/me/nimkoes/MyHelloController.java
@@ -1,25 +1,36 @@
 package me.nimkoes;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(method = RequestMethod.GET)
+@RestController
 public class MyHelloController {
 
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "hello";
-    }
+    @GetMapping("/hello?")
+    public String hello_case_1() { return "hello case 1"; }
 
-    @PostMapping
-    @ResponseBody
-    public String hi() {
-        return "hi";
-    }
+    @GetMapping("/hello/?")
+    public String hello_case_2() { return "hello case 2"; }
+
+    @GetMapping("/hello/??")
+    public String hello_case_3() { return "hello case 3"; }
+
+    @GetMapping("/hello/?/?")
+    public String hello_case_4() { return "hello case 4"; }
+
+    @GetMapping("/hello*")
+    public String hello_case_5() { return "hello case 5"; }
+
+    @GetMapping("/hello/*/test")
+    public String hello_case_6() { return "hello case 6"; }
+
+    @GetMapping("/hello/*/te*st")
+    public String hello_case_7() { return "hello case 7"; }
+
+    @GetMapping("/hello/**")
+    public String hello_case_8() { return "hello case 8"; }
+
+    @GetMapping("/hello/**/test")
+    public String hello_case_9() { return "hello case 9"; }
 
 }
--- src/main/test/me/nimkoes/MyHelloControllerTest.java
+++ src/main/test/me/nimkoes/MyHelloControllerTest.java
@@ -1,7 +1,7 @@
 package me.nimkoes;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,10 +20,18 @@ public class MyHelloControllerTest {
     @Test
     public void helloTest() throws Exception {
 
-        mockMvc.perform(get("/hello")).andExpect(status().isOk());                 // 200
-        mockMvc.perform(post("/hello")).andExpect(status().isMethodNotAllowed());  // 405
-        mockMvc.perform(get("/hi")).andExpect(status().isNotFound());              // 404
-        mockMvc.perform(post("/hi")).andExpect(status().isNotFound());             // 404
+        mockMvc.perform(get("/hello1"))                    .andExpect(content().string("hello case 1"));
+        mockMvc.perform(get("/hello!"))                    .andExpect(content().string("hello case 1"));
+        mockMvc.perform(get("/hello/z"))                   .andExpect(content().string("hello case 2"));
+        mockMvc.perform(get("/hello/nk"))                  .andExpect(content().string("hello case 3"));
+        mockMvc.perform(get("/hello/n/k"))                 .andExpect(content().string("hello case 4"));
+        mockMvc.perform(get("/hello"))                     .andExpect(content().string("hello case 5"));
+        mockMvc.perform(get("/helloTest"))                 .andExpect(content().string("hello case 5"));
+        mockMvc.perform(get("/hello/nimkoes/test"))        .andExpect(content().string("hello case 6"));
+        mockMvc.perform(get("/hello/nk/te_some_st"))       .andExpect(content().string("hello case 7"));
+        mockMvc.perform(get("/hello/nk/some"))             .andExpect(content().string("hello case 8"));
+        mockMvc.perform(get("/hello/nk/some/another"))     .andExpect(content().string("hello case 8"));
+        mockMvc.perform(get("/hello/nk/some/another/test")).andExpect(content().string("hello case 9"));
 
     }
 
