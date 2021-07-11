c0ff8b889c24f39ec344ebecb5daa263122429fe
nimkoes <xxxelppa@gmail.com>
2021-07-11 15:05:08 +0900

    3.1 요청 매핑하기 (handler method) part 5

--- src/main/java/me/nimkoes/MyHelloController.java
+++ src/main/java/me/nimkoes/MyHelloController.java
@@ -1,28 +1,16 @@
 package me.nimkoes;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyHelloController {
 
-    @GetMapping(value = "/hello", headers = HttpHeaders.AUTHORIZATION + "=" + "4321")
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public String hello() {
-        return "hello";
-    }
-
-    @GetMapping(value = "/hi", headers = "!" + HttpHeaders.AUTHORIZATION)
-    @ResponseBody
-    public String hi() {
-        return "hi";
-    }
-
-    @GetMapping(value = "/helloParam", params = "test")
-    @ResponseBody
-    public String helloParam() {
         return "hello";
     }
 
--- src/main/test/me/nimkoes/MyHelloControllerTest.java
+++ src/main/test/me/nimkoes/MyHelloControllerTest.java
@@ -1,14 +1,13 @@
 package me.nimkoes;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,36 +21,15 @@ public class MyHelloControllerTest {
     @Test
     public void helloTest() throws Exception {
 
-        mockMvc.perform(
-                get("/hello")
-                .header(HttpHeaders.AUTHORIZATION,"4321")
-            )
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(content().string("hello"))
-        ;
-
-        mockMvc.perform(
-                get("/hi")
-                .header(HttpHeaders.AUTHORIZATION,"1234")
-            )
-            .andDo(print())
-            .andExpect(status().isNotFound())
-        ;
-
-        mockMvc.perform(get("/hi"))
+        mockMvc.perform(head("/hello"))
             .andDo(print())
             .andExpect(status().isOk())
         ;
 
-        mockMvc.perform(
-                get("/helloParam")
-                .param("test", "1234")
-            )
+        mockMvc.perform(options("/hello"))
             .andDo(print())
             .andExpect(status().isOk())
         ;
 
     }
-
 }
