fbb0d6bde20fe809132c6835ff7725c166b43d6a
nimkoes <xxxelppa@gmail.com>
2021-07-13 21:58:26 +0900

    delete GetHelloMapping.java

--- src/main/java/me/nimkoes/MyHelloController.java
+++ src/main/java/me/nimkoes/MyHelloController.java
@@ -1,11 +1,14 @@
 package me.nimkoes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyHelloController {
 
-    @GetHelloMapping
+    @GetMapping
+    @RequestMapping
     public String hello() {
         return "hello";
     }
